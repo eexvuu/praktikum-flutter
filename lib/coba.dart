11e3ea4daa159cd8c3508f58bc16fcd3d2ca760c
usamah abdul matin <44694889+eexvuu@users.noreply.github.com>
2023-03-26 22:13:22 +0700

    menambahkan coba.dart

---
+++ lib/coba.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CobaGit extends StatefulWidget {
+  const CobaGit({super.key});
+
+  @override
+  State<CobaGit> createState() => _CobaGitState();
+}
+
+class _CobaGitState extends State<CobaGit> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Coba github')),
+    );
+  }
+}
